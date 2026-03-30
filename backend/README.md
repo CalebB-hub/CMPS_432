71f2ba87f3e6844d1a77408c5b32c9c3b55710d5
Ethan Hanks <ethanks@gmail.com>
2026-03-29 19:03:06 -0500

    created table generation code

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+# Setup
+1. create venv with python ```python -m venv venv```
+2. activate venv ```source venv/activate```
+3. install sqlalchemy ```pip install sqlalchemy```
--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,70 @@
-from sqlalchemy import
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Mapped, mapped_column
+from sqlalchemy.orm.properties import ForeignKey
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    pass
+
+def primary_key():
+    return mapped_column(primary_key=True, init=False)
+
+class User(Base):
+    __tablename__ = "User"
+
+    id: Mapped[int] = primary_key()
+    name: Mapped[str]
+    password: Mapped[str]
+
+class Item(Base):
+    __tablename__ = "Item"
+
+    id: Mapped[int] = primary_key()
+    name: Mapped[str]
+    path: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.id"))
+
+class Share(Base):
+    __tablename__ = "Share"
+
+    id: Mapped[int] = primary_key()
+    item_id: Mapped[int] = mapped_column(ForeignKey("Item.id"))
+    recipient_id: Mapped[int] = mapped_column(ForeignKey("User.id"))
+
+class Tag(Base):
+    __tablename__ = "Tag"
+
+    id: Mapped[int] = primary_key()
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.id"))
+    name: Mapped[str]
+
+
+class TagItem(Base):
+    __tablename__ = "TagItem"
+
+    id: Mapped[int] = primary_key()
+    tag_id: Mapped[int] = mapped_column(ForeignKey("Tag.id"))
+    item_id: Mapped[int] = mapped_column(ForeignKey("Item.id"))
+
+class TagTag(Base):
+    __tablename__ = "TagTag"
+
+    id: Mapped[int] = primary_key()
+    parent_id: Mapped[int] = mapped_column(ForeignKey("Tag.id"))
+    child_id: Mapped[int] = mapped_column(ForeignKey("Tag.id"))
+
+class Alias(Base):
+    __tablename__ = "Alias"
+
+    id: Mapped[int] = primary_key()
+    tag_id: Mapped[int] = mapped_column(ForeignKey("Tag.id"))
+    name: Mapped[str]
+
 DB_FILENAME = "db.sqlite"
 def init_db():
-
+    engine = create_engine(f"sqlite:///{DB_FILENAME}")
+    create_tables(engine)
     pass
+def create_tables(engine):
+    with engine.connect() as conn:
+        Base.metadata.create_all(conn)
+        conn.commit()
